13ca777113557637325ba981cc111656015e0909
Raemon <raemon777@gmail.com>
2026-01-23 17:13:47 -0800

    remove cat stuff

--- app/cat-eol-research/page.tsx
+++
@@ -1,6 +0,0 @@
-import CatEolResearchPage from './CatEolResearchPage'
-
-export default async function Page({searchParams}: {searchParams: Promise<{provider?: string}>}) {
-  const params = await searchParams
-  return <CatEolResearchPage searchParams={params} />
-}
